9bd5373b5166b369f5faf48c4f9c0ac78204eecb
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-06-12 23:57:25 +0300

    boardsmeet

--- boardmeets/base/models.py
+++ boardmeets/base/models.py
@@ -3,13 +3,19 @@ from django.db import models
 # Create your models here.
 class Organization(models.Model):
     organization_name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000, default=None)
     location = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=20)
     website = models.CharField(max_length=200)
     stuff_number = models.IntegerField()
+    address = models.CharField(max_length=255, default=None)
+    industry = models.CharField(max_length=255, default=None)
+    is_nonprofit = models.BooleanField(default=False)
+    established_date = models.DateField(default=None)
+    social_media_facebook = models.URLField(null=True, blank=True)
+    social_media_twitter = models.URLField(null=True, blank=True)
+    social_media_instagram = models.URLField(null=True, blank=True)
+    social_media_linkedin = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.organization_name
-    
-
-
--- boardmeets/base/urls.py
+++ boardmeets/base/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("organizations/", views.organization_list, name='org_list')
+    path("organizations/", views.organization_list, name='org_list'),
 ]
